dd04c3acd15ff8ac2a4575950990b2f241bd068b
Stuart McMillan <stuartmcmillan100@hotmail.com>
2023-01-27 17:26:39 +0000

    fix

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -2,11 +2,13 @@ from typing import List
 from pathlib import Path
 
 class Parser:
-    extensions = List[str]
-    extensions = []
+    extensions: List[str] = []
 
-    def valid_extensions(extension):
-        if extension.isinstance:
-            return True
-        else:
-            return False
+    def valid_extensions(self, extension):
+        return extension in self.extensions
+    
+    def parse(self, path, source, dest):
+        self.path = Path( path )
+        self.source = Path( source )
+        self.dest = Path( dest )
+        raise NotImplementedError
