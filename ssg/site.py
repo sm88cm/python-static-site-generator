0fd264523d39ee6e41a8b15b8033a9c46ad5201e
Stuart McMillan <stuartmcmillan100@hotmail.com>
2023-01-27 16:15:28 +0000

    fix

--- ssg/site.py
+++ ssg/site.py
@@ -10,8 +10,8 @@ class Site:
         directory = self.dest + "/" + path.relative_to(self.source)
         directory.mkdir( parents=True, exist_ok=True )
 
-    def build():
-        Path( self.dest ).mkdir( parents=True, exist_ok=True )
-        for path in self.source.rglob("*"):
-            if path == directory:
-                create_dir(path)
+    def build(self):
+        self.dest.mkdir( parents=True, exist_ok=True )
+        #for path in self.source.rglob("*"):
+        #    if path == directory:
+        #        create_dir(path)
