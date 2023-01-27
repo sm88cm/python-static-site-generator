18b357605cb555e823647c7c9112daa7fbc2b8fa
Stuart McMillan <stuartmcmillan100@hotmail.com>
2023-01-27 17:21:26 +0000

    fix

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -2,9 +2,8 @@ from typing import List
 from pathlib import Path
 
 class Parser:
-    
-    def __init__(self):
-        self.extensions = List[str] = []
+    extensions = List[str]
+    extensions = []
 
     def valid_extensions(extension):
         if extension.isinstance:
