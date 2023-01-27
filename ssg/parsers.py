39a150c332dd77b29f662d52e6fb04f98a936f6e
Stuart McMillan <stuartmcmillan100@hotmail.com>
2023-01-27 17:14:28 +0000

    fix

---
+++ ssg/parsers.py
@@ -0,0 +1,12 @@
+from typing import List
+from pathlib import Path
+
+class Parser:
+    extensions = List[str]
+
+    def valid_extensions(extension):
+        for e in self.extensions:
+            if e == extension:
+                return True
+            else:
+                return False
